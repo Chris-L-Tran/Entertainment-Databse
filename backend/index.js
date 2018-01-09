5e21d57a53139b7d5637661b84344ac81a532957
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-09 16:11:37 -0500

    created view files, added hbs and method override dependancies

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 // const Controller = require('./controllers/controller')
 const parser = require('body-parser')
 const cors = require('cors')
-
+const hbs = require('express-handlebars')
+const methodOverride = require('method-override'
+)
 const app = express()
 const PORT = 9000
 
@@ -10,7 +12,15 @@ app.use(cors())
 
 app.use(parser.urlencoded({extended: true}))
 app.use(parser.json())
+app.use(methodOverride('_method'))
+app.set('view engine', 'hbs')
 
+app.engine('.hbs', hbs({
+  extname: '.hbs',
+  partialsDir: 'views/',
+  layoutsDir: 'views/',
+  defaultLayout: 'layout-main'
+}))
 // app.use('/controller', Controller)
 
 app.listen(PORT, () => {
